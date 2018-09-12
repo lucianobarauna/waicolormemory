799b86e7a9a8f9f19bd2e71e2c5e44cd30d2a967
Luciano Baraúna <baraunaluciano@gmail.com>
2018-09-12 01:28:54 +0000

    create request axios

--- src/store/waicolormemory/actions.js
+++ src/store/waicolormemory/actions.js
@@ -2,9 +2,17 @@ import axios from 'axios'
 
 const actions = {
   fecthCards ({ commit }) {
-    axios.get('http://localhost:8080/json/cards.json')
+    axios.get('http://localhost:8080/json/cards.json', {
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    })
       .then(response => {
-        commit('SET_CARDS', response.data.cards)
+        setTimeout(() => {
+          commit('SET_CARDS', response.data.cards)
+        }, 1000)
       }).catch(e => {
         console.log(e)
       })
